7d2988e4ebc698f658b76ed0fdaf6bb642161ccf
reidstrange <reid.strange@gmail.com>
2021-08-27 08:53:06 -0500

    added xcode files
    Codepath iOS Prework

---
+++ Codepath iOS - Prework - Reid StrangeTests/Codepath_iOS___Prework___Reid_StrangeTests.swift
@@ -0,0 +1,33 @@
+//
+//  Codepath_iOS___Prework___Reid_StrangeTests.swift
+//  Codepath iOS - Prework - Reid StrangeTests
+//
+//  Created by Reid Strange on 8/27/21.
+//
+
+import XCTest
+@testable import Codepath_iOS___Prework___Reid_Strange
+
+class Codepath_iOS___Prework___Reid_StrangeTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
